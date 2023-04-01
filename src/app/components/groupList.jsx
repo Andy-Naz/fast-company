73f50b56d76d7e20a7c69d2a26ab8c3598727838
Andrey Nazarenko <skiminok86@gmail.com>
2023-03-31 21:18:25 -0600

    add: grouplist component pass two types of data

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -4,17 +4,28 @@ import PropTypes from "prop-types"
 const GroupList = ({ items, valueProperty, contentProperty, onItemSelect, selectedItem }) => {
     return (
         <ul className="list-group">
-            {Object.keys(items).map((item) => (
-                <li
-                    key={items[item][valueProperty]}
-                    className={"list-group-item" + (items[item] === selectedItem ? " active" : "")}
-                    onClick={() => onItemSelect(items[item])}
-                    role="button"
-                >
-                    {items[item][contentProperty]}
-                    {/* {console.log(items[item])} */}
-                </li>
-            ))}
+            {Array.isArray(items)
+                ? items.map((item) => (
+                    <li
+                        key={item[valueProperty]}
+                        className={"list-group-item" + (item === selectedItem ? " active" : "")}
+                        onClick={() => onItemSelect(item)}
+                        role="button"
+                    >
+                        {item[contentProperty]}
+                    </li>
+                ))
+                : Object.keys(items).map((item) => (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={"list-group-item" + (items[item] === selectedItem ? " active" : "")}
+                        onClick={() => onItemSelect(items[item])}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))
+            }
         </ul>
     )
 }
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -28,11 +28,14 @@ const Users = ({ users: allUsers, ...rest }) => {
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex)
     }
+
     const clearFilter = () => {
         setSelectedProf()
     }
 
-    const filteredUsers = selectedProf ? allUsers.filter((user) => user.profession === selectedProf) : allUsers
+    const filteredUsers = selectedProf
+        ? allUsers.filter((user) => JSON.stringify(user.profession) === JSON.stringify(selectedProf))
+        : allUsers
     const count = filteredUsers.length
 
     const userCrop = paginate(filteredUsers, currentPage, pageSize)
