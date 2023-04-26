cbdd9eda031fa07f31fa32cf58e8692feca26b9f
Andrey Nazarenko <skiminok86@gmail.com>
2023-04-26 09:31:56 -0600

    add: TextField component

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,16 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
+import TextField from "../components/textField"
 
 const Login = () => {
+    const [data, setData] = useState({ email: "", password: "" })
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }))
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(data)
+    }
+
     return (
-        <form action="">
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
-            </div>
-            <div>
-                <label htmlFor="password">Пароль</label>
-                <input type="password" id="password" />
-            </div>
+        <form onSubmit={handleSubmit}>
+            <TextField label="Электронная почта" name="email" value={data.email} onChange={handleChange} />
+            <TextField label="Пароль" type="password" name="password" value={data.password} onChange={handleChange} />
+            <button type="submit">Отправить</button>
         </form>
     )
 }
