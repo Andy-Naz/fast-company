1ab7cfc23507932418a42b5defb94cb945ee439a
Andrey Nazarenko <skiminok86@gmail.com>
2023-06-14 22:38:46 +0300

    correct TextAreaField

--- src/app/components/common/form/textAreaField.jsx
+++ src/app/components/common/form/textAreaField.jsx
@@ -5,26 +5,24 @@ const TextAreaField = ({ label, name, value, onChange, error }) => {
     const handleChange = ({ target }) => {
         onChange({ name: target.name, value: target.value })
     }
-
     const getInputClasses = () => {
         return "form-control" + (error ? " is-invalid" : "")
     }
 
     return (
         <div className="mb-4">
-            <label htmlFor={name}>{label}</label>
+            <label htmlFor={name}> {label}</label>
             <div className="input-group has-validation">
                 <textarea id={name} name={name} value={value} onChange={handleChange} className={getInputClasses()} />
-                {error && <div className="invalid-feedback">{error}</div>}
+
+                {error && <div className="invalid-feedback ">{error}</div>}
             </div>
         </div>
     )
 }
-
-// TextAreaField.default = {
-//     type: "text"
-// }
-
+TextAreaField.defaultProps = {
+    type: "text"
+}
 TextAreaField.propTypes = {
     label: PropTypes.string,
     type: PropTypes.string,
