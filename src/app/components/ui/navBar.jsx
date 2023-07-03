87931ad3448d297989747b92fa43da16c9eb2595
Andrey Nazarenko <skiminok86@gmail.com>
2023-07-03 12:25:22 +0300

    add: NavBar styles

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -6,30 +6,32 @@ import NavProfile from "./navProfile"
 const NavBar = () => {
     const { currentUser } = useAuth()
     return (
-        <nav className="navbar bg-light mb-3">
+        <nav className="navbar navbar-expand-lg navbar-light mb-3" style={{ backgroundColor: "#e3f2fd" }}>
             <div className="container-fluid">
-                <ul className="nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">
-                            Main
-                        </Link>
-                    </li>
-                    {currentUser && (
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/users">
-                                Users
+                            <Link className="nav-link" to="/">
+                                Main
                             </Link>
                         </li>
-                    )}
-                </ul>
-                <div className="d-flex">
-                    {currentUser ? (
-                        <NavProfile />
-                    ) : (
-                        <Link className="nav-link" to="/login">
-                            Login
-                        </Link>
-                    )}
+                        {currentUser && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/users">
+                                    Users
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                    <div className="d-flex">
+                        {currentUser ? (
+                            <NavProfile />
+                        ) : (
+                            <Link className="nav-link" to="/login">
+                                Login
+                            </Link>
+                        )}
+                    </div>
                 </div>
             </div>
         </nav>
