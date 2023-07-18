354b77bda23341b7abaf836e38a941fa31832854
Andrey Nazarenko <skiminok86@gmail.com>
2023-07-18 11:49:49 +0300

    add: data update - qualities

--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,13 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import Quality from "./quality"
-import { useSelector } from "react-redux"
-import { getQualitiesByIds, getQualitiesLoadingStatus } from "../../../store/qualities"
+import { useDispatch, useSelector } from "react-redux"
+import { getQualities, getQualitiesLoadingStatus, loadQualitiesList } from "../../../store/qualities"
 
 const QualitiesList = ({ qualities }) => {
+    const dispatch = useDispatch()
+
     const isLoading = useSelector(getQualitiesLoadingStatus())
+    const qualitiesState = useSelector(getQualities())
+    const qualitiesList = qualitiesState.filter((quality) => qualities.includes(quality._id))
+
+    useEffect(() => {
+        dispatch(loadQualitiesList())
+    }, [])
+
     if (!isLoading) {
-        const qualitiesList = useSelector(getQualitiesByIds(qualities))
         return (
             <>
                 {qualitiesList.map((qual) => (
--- src/app/store/qualities.js
+++ src/app/store/qualities.js
@@ -6,7 +6,8 @@ const qualitiesSlice = createSlice({
     initialState: {
         entities: null,
         isLoading: true,
-        error: null
+        error: null,
+        lastFetch: null
     },
     reducers: {
         qualitiesRequested: (state) => {
@@ -14,6 +15,7 @@ const qualitiesSlice = createSlice({
         },
         qualitiesReceived: (state, action) => {
             state.entities = action.payload
+            state.lastFetch = Date.now()
             state.isLoading = false
         },
         qualitiesRequestFailed: (state, action) => {
@@ -26,23 +28,50 @@ const qualitiesSlice = createSlice({
 const { reducer: qualitiesReducer, actions } = qualitiesSlice
 const { qualitiesRequested, qualitiesReceived, qualitiesRequestFailed } = actions
 
-export const loadQualitiesList = () => async (dispatch) => {
-    dispatch(qualitiesRequested())
-    try {
-        const { content } = await qualitiesService.get()
-        dispatch(qualitiesReceived(content))
-    } catch (error) {
-        dispatch(qualitiesRequestFailed(error.message))
+function isOutdated(date) {
+    if (Date.now() - date > 10 * 60 * 1000) {
+        return true
+    }
+    return false
+}
+
+export const loadQualitiesList = () => async (dispatch, getState) => {
+    const { lastFetch } = getState().qualities
+    if (isOutdated(lastFetch)) {
+        dispatch(qualitiesRequested())
+        try {
+            const { content } = await qualitiesService.get()
+            dispatch(qualitiesReceived(content))
+        } catch (error) {
+            dispatch(qualitiesRequestFailed(error.message))
+        }
     }
 }
 
 export const getQualities = () => (state) => state.qualities.entities
 export const getQualitiesLoadingStatus = () => (state) => state.qualities.isLoading
-export const getQualitiesByIds = (Ids) => (state) => {
-    if (state.qualities.entities) {
-        return state.qualities.entities.filter((quality) => Ids.includes(quality._id))
-    }
-    return []
-}
+
+// export const getQualitiesByIds = (Ids) => (state) => {
+//     if (state.qualities.entities) {
+//         return state.qualities.entities.filter((quality) => Ids.includes(quality._id))
+//     }
+//     return []
+// }
+
+// export const getQualitiesByIds = (qualitiesIds) => (state) => {
+//     if (state.qualities.entities) {
+//         const qualitiesArray = []
+//         for (const qualId of qualitiesIds) {
+//             for (const quality of state.qualities.entities) {
+//                 if (quality._id === qualId) {
+//                     qualitiesArray.push(quality)
+//                     break
+//                 }
+//             }
+//         }
+//         return qualitiesArray
+//     }
+//     return []
+// }
 
 export default qualitiesReducer
