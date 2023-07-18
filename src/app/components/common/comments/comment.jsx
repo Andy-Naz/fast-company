35aae4946e65a03e690e6167173c1cfa6b01d0c1
Andrey Nazarenko <skiminok86@gmail.com>
2023-07-18 12:51:11 +0300

    add: users in redux

--- src/app/components/common/comments/comment.jsx
+++ src/app/components/common/comments/comment.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { displayDate } from "../../../utils/displayDate"
-import { useUser } from "../../../hooks/useUsers"
 import { useAuth } from "../../../hooks/useAuth"
+import { useSelector } from "react-redux"
+import { getUserById } from "../../../store/users"
 
 const Comment = ({ content, created_at: created, _id: id, userId, onRemove }) => {
-    const { getUserById } = useUser()
     const { currentUser } = useAuth()
-    const user = getUserById(userId)
-
+    const user = useSelector(getUserById(userId))
     return (
         <div className="bg-light card-body  mb-3">
             <div className="row">
