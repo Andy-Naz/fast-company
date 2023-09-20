e22aacffc9d610d0b44ff301835e347782a7ffa9
Andrey Nazarenko <skiminok86@gmail.com>
2023-09-20 18:04:32 +0300

    fix cors issues

--- server/app.js
+++ server/app.js
@@ -2,17 +2,19 @@ const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
 const chalk = require("chalk")
+const cors = require("cors")
 const initDatabase = require("./startUp/initDatabase")
 const routes = require("./routes")
 
 const app = express()
 
-const PORT = config.get("port") ?? 8080
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.use("/api", routes)
+
+const PORT = config.get("port") ?? 8080
 
 // if (process.env.NODE_ENV === "production") {
 //     console.log("Production")
